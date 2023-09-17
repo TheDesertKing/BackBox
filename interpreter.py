9d530af2f19d3661e1a223ef0f07bc1ff50b393e
TheDesertKing <aammiitt11001100@gmail.com>
2023-09-18 02:17:43 +0300

    firstosorsecond

--- interpreter.py
+++ interpreter.py
@@ -4,23 +4,35 @@ def parse_commands_file(filename):
     with open(filename,'r') as cfile:
         commands_data = cfile.read()
     commands_json_list = json.loads(commands_data)
-    print(commands_json_list)
+    return commands_json_list
 
 def get_command_props(c):
-    props = {}
-    props.is_save = c.saveFlag
-    props.is_status = c.statusFlag
-    props.is_append = c.outputAppendToFile
-    props.file_perm = c.saveToFilePermissions
-    props.ctype = c.command_TYPE
-    props.hide_output = c.hide_OUTPUT
-    props.desc = c.description
-    props.waitfor = c.wait_FOR
+    duplicate_keys = ['timeout','sleep','status','condition','command','queue']
+    props = {key: c[key] for key in duplicate_keys}
+    props["is_save"] = c["saveFlag"]
+    props["is_status"] = c["statusFlag"]
+    props["is_append"] = c["outputAppendToFile"]
+    props["file_perm"] = c["saveToFilePermissions"]
+    props["ctype"] = c["command_TYPE"]
+    props["hide_output"] = c["hide_OUTPUT"]
+    props["desc"] = c["description"]
+    props["waitfor"] = c["wait_FOR"]
+    props["save"] = c["save_OUTPUT"]
+    props["save_type"] = c["output_TYPE"]
+    props["error_msg"] = c["error_MESSAGE"]
+
     return props
 
 
+def construct_line(props):
+    if props["ctype"] == internal
+
+
 def main():
-    
+    command_list = parse_commands_file('yasers')
+    for command in command_list:
+        props = get_command_props(command)
+        line = construct_line(props)
     pass
 
 
