0d804ff08eb6ef4e61e0de0240cc2cb27fc781ac
amit-eshel <amit-eshel@backbox.com>
2024-01-17 09:47:49 +0200

    icyget: add automatic vim opening

--- icyget.py
+++ icyget.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from sys import argv,exit
+import sys
+from subprocess import call
 from importIC import import_signature
 from translator import convert_icc_to_icy
 
@@ -7,14 +8,16 @@ from translator import convert_icc_to_icy
 def validate_argv(argv):
     if len(argv) < 2:
         print(f"missing IntelliCheck search\npython3 importIC.py Signaure Search")
-        exit(61)
+        sys.exit(61)
 
 
 def main():
-    validate_argv(argv)
-    signature_file_path = import_signature(argv[1:])['sig_file_path']
-    convert_icc_to_icy(signature_file_path)
+    validate_argv(sys.argv)
 
+    signature_file_path = import_signature(sys.argv[1:])['sig_file_path']
+    file_path = convert_icc_to_icy(signature_file_path)
+
+    call(['vim',file_path])
 
 
 main()
